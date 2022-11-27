95f7432c012abb9ac36c581bc8b898588e034ba6
chy0503 <202015006@kyonggi.ac.kr>
2022-11-27 12:40:25 +0900

    :sparkles: level function complete

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -1,23 +1,13 @@
 package com.example.sudoku;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Chronometer;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TableLayout;
-import android.widget.TableRow;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -26,26 +16,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
-//        ImageView IV1 = findViewById(R.id.startAfter1);
-//        LinearLayout IV2 = findViewById(R.id.startAfter2);
+        Button start = findViewById(R.id.startB);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                screenSet("level");
+            }
+        });
+        TextView close = (TextView) findViewById(R.id.closeB);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                screenSet("main");
+            }
+        });
+    }
 
-//        Button start = findViewById(R.id.startB);
-//        start.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                IV1.setVisibility(View.VISIBLE);
-//                IV2.setVisibility(View.VISIBLE);
-//            }
-//        });
-//
-//        TextView close = findViewById(R.id.closeB);
-//        close.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                IV1.setVisibility(View.GONE);
-//                IV2.setVisibility(View.GONE);
-//            }
-//        });
+    public void screenSet(String screen) {
+        TextView MS1 = findViewById(R.id.mainS1);
+        LinearLayout MS2 = findViewById(R.id.mainS2);
+        ImageView LS1 = findViewById(R.id.levelS1);
+        LinearLayout LS2 = findViewById(R.id.levelS2);
+        switch(screen) {
+            case "main":
+                MS1.setVisibility(View.VISIBLE);
+                MS2.setVisibility(View.VISIBLE);
+                LS1.setVisibility(View.GONE);
+                LS2.setVisibility(View.GONE);
+                break;
+            case "level":
+                MS1.setVisibility(View.GONE);
+                MS2.setVisibility(View.GONE);
+                LS1.setVisibility(View.VISIBLE);
+                LS2.setVisibility(View.VISIBLE);
+        }
     }
 
     public void levelClick(View v) {
@@ -63,5 +67,9 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         startActivity(intent);
+        screenSet("main");
     }
 }
+
+// SudokuActivity 만들기
+// level screen, activity 분리
