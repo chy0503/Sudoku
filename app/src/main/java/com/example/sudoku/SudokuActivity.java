409a7eb9143b70eb094321e13c7483e2ba39ecf8
chy0503 <202015006@kyonggi.ac.kr>
2022-12-06 05:38:39 +0900

    :sparkles: add reset function

--- app/src/main/java/com/example/sudoku/SudokuActivity.java
+++ app/src/main/java/com/example/sudoku/SudokuActivity.java
@@ -25,21 +25,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SudokuActivity extends AppCompatActivity {
+    private final int DYNAMIC_VIEW_ID = 0x8000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sudoku);
         getSupportActionBar().hide();
 
+        Intent intent = getIntent();
+        double level = intent.getExtras().getDouble("level");
+        TableLayout table = (TableLayout)findViewById(R.id.tableLayout);
+        bordGenerate(level);
+
         // 스톱워치 만들기
         Chronometer chronometer = findViewById(R.id.chronometer);
         chronometer.setBase(SystemClock.elapsedRealtime());
         chronometer.start();
 
-        // 스도쿠 table 만들기
-        Intent intent = getIntent();
-        double level = intent.getExtras().getDouble("level");
+        Button resetB = findViewById(R.id.resetB);
+        resetB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                table.removeAllViews();
+                bordGenerate(level);
+            }
+        });
+    }
 
+    public void bordGenerate(double level) {
         TableLayout table = (TableLayout)findViewById(R.id.tableLayout);
         table.setPadding(15, 15, 15, 15);
         TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(
@@ -55,6 +69,7 @@ public class SudokuActivity extends AppCompatActivity {
             for (int j = 0; j < 9; j++) {
                 buttons[i][j] = new CustomButton(this, i, j);
                 buttons[i][j].setLayoutParams(layoutParams);
+//                buttons[i][j].setId(DYNAMIC_VIEW_ID + numButton);
                 if (Math.random() >= level) {
                     buttons[i][j].set(this, board.get(i, j));
                     buttons[i][j].setClickable(false);
@@ -70,7 +85,7 @@ public class SudokuActivity extends AppCompatActivity {
                             // 커스텀 다이얼로그의 결과를 출력할 TextView를 매개변수로 같이 넘겨준다.
 //                            customDialog.callFunction(main_label);
                             int num = customDialog.callFunction();
-//                            buttons[i][j].set(this, num);
+//                            buttons[i][j].set(this, num); // 몰라잉 시발
                         }
                     });
 
@@ -103,7 +118,6 @@ public class SudokuActivity extends AppCompatActivity {
 // 배열버튼 넘기고 numpad 반환
 // 충돌 검사
 // 메모
-// reset
 // clear
 // 3*3마다 진한 선
 
